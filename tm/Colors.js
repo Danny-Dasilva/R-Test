44a0336911c262161a828fbc7576d974310893a7
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-02-06 19:14:23 -0500

    basic ui

---
+++ tm/Colors.js
@@ -0,0 +1,8 @@
+
+let colors = { 
+    black: "#2D3436",
+    blue: "#34A6D9",
+    lightBlue: "#A7CBD9",
+    white: "#FFFFFF",
+}
+export default colors 
---
+++ tm/tempData.js
@@ -0,0 +1,69 @@
+let tempData = [
+    {
+        name: "Plan a Trip",
+        color: '#24A6D9',
+        todos: [
+            {
+                title: "Book FLight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: true
+            },
+        ]
+    },
+    {
+        name: "Errands",
+        color: '#8022D9',
+        todos: [
+            {
+                title: "Buy Flight",
+                completed: true
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: true
+            },
+        ]
+    },
+    {
+        name: "Birthday Party",
+        color: '#8022D9',
+        todos: [
+            {
+                title: "Buy Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: false
+            },
+            {
+                title: "Reserve Room",
+                completed: false
+            },
+            {
+                title: "Pack Luggage",
+                completed: true
+            },
+        ]
+    }
+]
+export default tempData
